e8f316d7b933b7e86d0a6987ce8b80efc6e55160
brianxt6 <brianace674@gmail.com>
2025-04-21 09:34:14 -0500

    Base de datos

--- sistema2.sql
+++ sistema2.sql
@@ -3,9 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-04-2025 a las 03:10:41
--- Tiempo de generación: 17-04-2025 a las 00:20:25
->>>>>>> 2d9fb6e (Volviendo a la version 7bff)
+-- Tiempo de generación: 21-04-2025 a las 16:33:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.1.25
 
@@ -39,32 +37,21 @@ CREATE TABLE `resumen_solicitud` (
   `estado_jefe` varchar(255) NOT NULL,
   `nota_sistemas` varchar(255) NOT NULL,
   `estado_sistemas` varchar(255) NOT NULL,
-<<<<<<< HEAD
   `aprobacion` varchar(250) NOT NULL,
   `asesor` varchar(100) DEFAULT NULL,
-  `nombre_usuario` varchar(100) DEFAULT NULL
-=======
-  `aprobacion` varchar(250) NOT NULL
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nombre_usuario` varchar(100) DEFAULT NULL,
+  `evaluador` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `resumen_solicitud`
 --
 
-<<<<<<< HEAD
-INSERT INTO `resumen_solicitud` (`id_solicitud`, `usuario_id`, `titulo`, `fecha`, `nota_usuario`, `nota_jefe`, `estado_jefe`, `nota_sistemas`, `estado_sistemas`, `aprobacion`, `asesor`, `nombre_usuario`) VALUES
-(16, 23, 'Permisos celular\r\ncelular: Brian', '17/04/2025 00:17:33', 'Permisos', 'Rechazado por que no hay celulares', 'Rechazado', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', NULL, 'Brian Acevedo'),
-(17, 23, 'Permisos Pimovi\r\nusuario: brian.acevedo', '17/04/2025 09:03:25', 'Ruta pimovi/pedidos/autorizados', 'Aprobado solo ruta de pedidos', 'Aprobado', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', NULL, 'Brian Acevedo');
-=======
-INSERT INTO `resumen_solicitud` (`id_solicitud`, `usuario_id`, `titulo`, `fecha`, `nota_usuario`, `nota_jefe`, `estado_jefe`, `nota_sistemas`, `estado_sistemas`, `aprobacion`) VALUES
-(1, 23, 'Permisos Siesa', '16/04/2025', 'Permisos / Facturador / Notas Credito\r\nusuario: brian.acevedo', 'Aprobado', 'Aprobado Boton', 'Se asignan los permisos', 'Aprobado', 'Permisos asignados usuario brian.acevedo por favor validar'),
-(2, 23, 'Permisos Siesa', '16/04/2025', 'Permisos de Cartera Almacen 250 / Sede 120', 'Aprobado', 'Aprobado', 'Aprobado', 'Aprobado', 'Aprobado'),
-(3, 23, 'dfsa', '17/04/2025', '', 'Pendiente Jefe', 'Pendiente Estado J', 'Pendiente Sistemas', 'Pendiente estado s', 'Pendiente'),
-(4, 23, 'TEST 16-04-2025 5:12', '17/04/2025', '', 'Pendiente Jefe', 'Pendiente Estado J', 'Pendiente Sistemas', 'Pendiente estado s', 'Pendiente'),
-(5, 23, '123456', '17/04/2025', '123456', 'Pendiente Jefe', 'Pendiente Estado J', 'Pendiente Sistemas', 'Pendiente estado s', 'Pendiente'),
-(6, 23, 'Permisos Siesa\r\nusuario: brian.acevedo', '17/04/2025', 'Permisos notas/credito/aprobado', 'Pendiente Jefe', 'Pendiente Estado J', 'Pendiente Sistemas', 'Pendiente estado s', 'Pendiente'),
-(7, 23, 'Permisos Pagina Tropi', '16/04/2025', 'Fecha 16/04/2025 Facturacion/Permiso/Facturas', 'Pendiente Jefe', 'Pendiente Estado J', 'Pendiente Sistemas', 'Pendiente estado s', 'Pendiente');
->>>>>>> 2d9fb6e (Volviendo a la version 7bff)
+INSERT INTO `resumen_solicitud` (`id_solicitud`, `usuario_id`, `titulo`, `fecha`, `nota_usuario`, `nota_jefe`, `estado_jefe`, `nota_sistemas`, `estado_sistemas`, `aprobacion`, `asesor`, `nombre_usuario`, `evaluador`) VALUES
+(16, 23, 'Permisos celular\r\ncelular: Brian', '17/04/2025 00:17:33', 'Permisos', 'Rechazado por que no hay celulares', 'Rechazado', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', '26', 'Brian Acevedo', '27'),
+(17, 23, 'Permisos Pimovi\r\nusuario: brian.acevedo', '17/04/2025 09:03:25', 'Ruta pimovi/pedidos/autorizados', 'Aprobado solo ruta de pedidos', 'Aprobado', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', '26', 'Brian Acevedo', '27'),
+(18, 23, 'Pagina tropi\r\nbrian.acevedo', '17/04/2025 21:59:04', 'Facturas/Cuentas/Clientes', 'Pendiente Jefe', 'Pendiente', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', NULL, 'Brian Acevedo', NULL),
+(19, 23, 'Permisos pagina tropi\r\nusuario:Brian Acevedo', '17/04/2025 22:47:26', 'Facturas/Pedidos/Ordenes 10:47PM\r\n', 'Pendiente Jefe', 'Pendiente', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', NULL, 'Brian Acevedo', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,7 +88,7 @@ CREATE TABLE `trabajo_grado` (
 --
 
 INSERT INTO `trabajo_grado` (`id_trabajo`, `titulo`, `autor1`, `autor2`, `fecha_trabajo`, `resumen`, `abstract`, `archivo`, `pclave1`, `pclave2`, `pclave3`, `pclave4`, `pclave5`, `pclave6`, `usuario_id`, `asesor`, `evaluador`, `estado_asesor`, `conclusion_asesor`, `estado_evaluador`, `conclusion_evaluador`) VALUES
-(14, 'Inteligencia Artificial', 'Brian Acevedo', 'Arley Lorenzo', '2024-11-25', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ad.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt vero labore ut cum molestias cupiditate aperiam laboriosam rerum molestiae possimus, architecto sequi aliquid. Accusamus', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt vero labore ut cum molestias cupiditate aperiam laboriosam rerum molestiae possimus, architecto sequi aliquid. Accusamus amet ipsum harum! Neque, eos est!.', 'uploads/20241125211019_Test123.pdf', 'IA', 'GOOGLE', 'MICROSOFT', 'CHAT', 'GPT', 'NVIDIA', 23, '', '', 'Pendiente', 'APROBADO 11:27PM POR CUMPLE LOS REQUISITOS', 'Rechazado', 'RECHAZADO 11:28PM'),
+(14, 'Inteligencia Artificial', 'Brian Acevedo', 'Arley Lorenzo', '2024-11-25', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ad.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt vero labore ut cum molestias cupiditate aperiam laboriosam rerum molestiae possimus, architecto sequi aliquid. Accusamus', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt vero labore ut cum molestias cupiditate aperiam laboriosam rerum molestiae possimus, architecto sequi aliquid. Accusamus amet ipsum harum! Neque, eos est!.', 'uploads/20241125211019_Test123.pdf', 'IA', 'GOOGLE', 'MICROSOFT', 'CHAT', 'GPT', 'NVIDIA', 23, '26', '27', 'Pendiente', 'APROBADO 11:27PM POR CUMPLE LOS REQUISITOS', 'Rechazado', 'RECHAZADO 11:28PM'),
 (15, 'Investigacion de Marketing', 'Mariano Castro', 'Samuel Alvarez', '2024-11-25', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit voluptate pariatur ipsam tenetur ducimus quibusdam laudantium corrupti, placeat accusamus?.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim doloribus, commodi facilis officiis impedit quisquam maxime. Dignissimos ipsum fugiat quo!', 'uploads/20241125211704_Test2.pdf', 'IA', 'KPG', 'KPI', 'ANALISIS', 'MARKET', 'CAP', 24, '26', '27', 'Aprobado', 'APROBADO JACINTO LINARES 123456', 'Aprobado', 'CONFIRMADO EMILIO');
 
 -- --------------------------------------------------------
@@ -162,11 +149,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `resumen_solicitud`
 --
 ALTER TABLE `resumen_solicitud`
-<<<<<<< HEAD
-  MODIFY `id_solicitud` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-=======
-  MODIFY `id_solicitud` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
->>>>>>> 2d9fb6e (Volviendo a la version 7bff)
+  MODIFY `id_solicitud` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajo_grado`
