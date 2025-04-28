fed5fc00c03a590c5364b4a916b53225aacbded5
brianxt6 <brianace674@gmail.com>
2025-04-28 00:07:34 -0500

    Base de datos actulizada

--- sistema2.sql
+++ sistema2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-04-2025 a las 03:47:10
+-- Tiempo de generación: 28-04-2025 a las 07:06:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.1.25
 
@@ -52,9 +52,7 @@ INSERT INTO `resumen_solicitud` (`id_solicitud`, `usuario_id`, `titulo`, `fecha`
 (25, 23, 'Permiso pagina Tropi\r\nusuarrio: brian.acevedo', '21/04/2025 09:38:38 PM', 'Ruta permisos/facturacion/escritura', 'Aprobado por cartera', 'Aprobado', 'Permisos asignados', 'Aprobado', 'Pendiente', '23', 'Brian Acevedo', '27', '21/04/2025 09:39:47 PM'),
 (26, 23, 'Permiso pimovi\r\nusuario: steven.ramirez', '21/04/2025 09:41:18 PM', 'Ruta pedidos/compras/aprobacion', 'Permisos solo para gerentes', 'Rechazado', 'Permisos solo para gerentes', 'Rechazado', 'Pendiente', '23', 'Brian Acevedo', '27', '21/04/2025 09:42:35 PM'),
 (27, 23, 'permisos siesa\r\nusuario: steven.ramirez', '21/04/2025 10:33:03 PM', 'ruta: pedidos/cancelados', 'Aprobado por gerencia', 'Aprobado', 'Sistemas 003', 'Aprobado', 'Pendiente', '23', 'Brian Acevedo', '27', '21/04/2025 11:13:35 PM'),
-(28, 24, 'Permisos siesa\r\nusuario: steven.ramirez', '21/04/2025 11:30:15 PM', 'Ruta: pedidos/cancelados', 'Aprobado por logistica', 'Aprobado', 'Permisos asignados', 'Aprobado', 'Pendiente', '23', 'Steven Ramirez', '27', '21/04/2025 11:31:28 PM'),
-(29, 23, 'Plataforma Pimovi\r\nusuario brian.acevedo', '22/04/2025 12:32:15 AM', 'Ruta: pedidos/productos/Autorizacion', 'Aprobado por Logistica', 'Aprobado', 'Solucionado', 'Aprobado', 'Pendiente', '23', 'Brian Acevedo', '27', '22/04/2025 12:33:16 AM'),
-(30, 23, 'Plataforma pimovi\r\nusuario: brian.martin', '23/04/2025 08:10:18 PM', 'Ruta pedidos/cancelados', 'Aprobado 001', 'Aprobado', 'Pendiente Sistemas', 'Pendiente', 'Pendiente', '23', 'Brian Acevedo', '27', NULL);
+(35, 23, 'Permisos area facturacion', '27/04/2025 09:49:29 AM', '<strong>Plataforma: </strong>Pimovi<strong> Usuario: </strong>brian.acevedo<strong> Nota: </strong>Ruta pedidos/aprobados', 'Aprobado por logistica', 'Aprobado', 'Permiso solo para gerentes', 'Rechazado', 'Pendiente', '23', 'Brian Acevedo', '27', '27/04/2025 09:52:41 AM');
 
 -- --------------------------------------------------------
 
@@ -106,21 +104,20 @@ CREATE TABLE `usuarios` (
   `apellidos` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `rol` varchar(100) NOT NULL
+  `rol` varchar(100) NOT NULL,
+  `cc` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `email`, `password`, `rol`) VALUES
-(23, 'Brian', 'Acevedo', 'brianacevedo2001@gmail.com', '$2y$10$Y5zHtMCJpGuMg4iTHxRWHeAf500I1XBTsIybAizwaxl24.H1JBjte', '1'),
-(24, 'Steven', 'Ramirez', 'brianace674@gmail.com', '$2y$10$pUUWqT4fH9QhqRhvIGzwrulC3nYNuPzHaGCOEsI9r99lVkS9dsw0.', '1'),
-(25, 'Steven', 'Ramirez', 'comite@uc.edu.co', '$2y$10$nBC6T5eU92RVVeg4sF5Kt.ziJt2czIIaYiWVBPCleX8AwiircWdA6', '2'),
-(26, 'Dorian', 'Trujillo', 'jefe@grupotropi.com', '$2y$10$A4erWRvI8mJwtCb4XgVpoO/4iqifeerm6mJWAKWeC42s6a7FIx1H6', '3'),
-(27, 'Juan', 'Beltran', 'sistemas@grupotropi.com', '$2y$10$HRbjLC2cjHfw/AtfrEfZ5OayZtUuNOQRRQpbGvtR5b/.YtiCBzgiy', '4'),
-(30, 'Albeiro', 'Ortiz', 'admin@grupotropi.com', '$2y$10$plTR4N68.5n0N7ui9CtWtOnWASi2CmpmlY7GB4W3JjV6UpFeeClZm', '5'),
-(31, 'Pedro', 'Perez', 'pedro@gmail.com', '$2y$10$LVRmLnSjpntx5SuiOAVL3eU954YYFIxKErAymynPH2wBl4iTq4goG', '4');
+INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `email`, `password`, `rol`, `cc`) VALUES
+(23, 'Brian', 'Acevedo', 'brianacevedo2001@gmail.com', '$2y$10$Y5zHtMCJpGuMg4iTHxRWHeAf500I1XBTsIybAizwaxl24.H1JBjte', '1', '1004680'),
+(24, 'Steven', 'Ramirez', 'brianace674@gmail.com', '$2y$10$pUUWqT4fH9QhqRhvIGzwrulC3nYNuPzHaGCOEsI9r99lVkS9dsw0.', '1', '1004689'),
+(26, 'Dorian', 'Trujillo', 'jefe@grupotropi.com', '$2y$10$A4erWRvI8mJwtCb4XgVpoO/4iqifeerm6mJWAKWeC42s6a7FIx1H6', '3', '1004681'),
+(27, 'Juan', 'Beltran', 'sistemas@grupotropi.com', '$2y$10$HRbjLC2cjHfw/AtfrEfZ5OayZtUuNOQRRQpbGvtR5b/.YtiCBzgiy', '4', '1004682'),
+(30, 'Albeiro', 'Ortiz', 'admin@grupotropi.com', '$2y$10$plTR4N68.5n0N7ui9CtWtOnWASi2CmpmlY7GB4W3JjV6UpFeeClZm', '5', '1004683');
 
 --
 -- Índices para tablas volcadas
@@ -152,7 +149,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `resumen_solicitud`
 --
 ALTER TABLE `resumen_solicitud`
-  MODIFY `id_solicitud` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_solicitud` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajo_grado`
@@ -164,7 +161,7 @@ ALTER TABLE `trabajo_grado`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
